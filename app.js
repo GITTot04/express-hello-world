bfc821f7969e212afc16db611dba7bad39dc7d8c
GITTot04 <112081156+GITTot04@users.noreply.github.com>
2022-09-26 10:04:11 +0200

    sendfile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send("03a_express_eksempel\public\index.html"));
+app.get("/", (req, res) => res.type('html').sendFile('03a_express_eksempel\public\index.html'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
