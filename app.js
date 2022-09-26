ca4ff37338f9e2026708f3dea72d6883fe69fc4a
GITTot04 <112081156+GITTot04@users.noreply.github.com>
2022-09-26 10:50:14 +0200

    uden 03aeksempel

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.render('03a_express_eksempel\public\index.html'));
+app.get("/", (req, res) => res.render('public\index.html'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
