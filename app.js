36b47e27d4c028e21efde4f212d9686fac381bef
GITTot04 <112081156+GITTot04@users.noreply.github.com>
2022-09-26 09:41:48 +0200

    html dokument vælges via src

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-/*
-app.get("/", (req, res) => res.type('html').send(html));
+
+app.get("/", (req, res) => res.type('html').send(src = "03a_express_eksempel\public\index.html"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-*/
+
 
 const html = `
 <!DOCTYPE html>
